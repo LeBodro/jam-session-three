cd77b3c46862457e30799a999fc084a83c8c7129
Boud <maxime.boudreault.0@gmail.com>
2019-04-27 16:01:40 -0400

    Remove module offset

--- Assets/Module.cs
+++ Assets/Module.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Module : MonoBehaviour, IDraggable
+public class Module : MonoBehaviour, IDraggable, IPointerUpHandler
 {
-    Vector3 dragPositionOffset;
-
     event System.Action<Module, PointerEventData> _onDragRelease = delegate { };
     public event System.Action<Module, PointerEventData> OnDragRelease
     {
@@ -17,17 +15,21 @@ public class Module : MonoBehaviour, IDraggable
     public void OnBeginDrag(PointerEventData eventData)
     {
         // Note the position on the module where the mouse is relative to the transform
-        dragPositionOffset = MouseHelper.toWorldPosition(eventData.position) - transform.position;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         // Adjust the object position to take the original offset into account in order to feel like a draggable item
-        transform.position = MouseHelper.toWorldPosition(eventData.position) - dragPositionOffset;
+        transform.position = MouseHelper.toWorldPosition(eventData.position);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         _onDragRelease(this, eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
     }
 }
