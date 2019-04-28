daab815bcecc912dd50fe101deb0693868aeaf5b
Boud <maxime.boudreault.0@gmail.com>
2019-04-28 18:16:51 -0400

    Add button and simplify sprite importation

---
+++ Assets/Editor/ImportSettingsHelper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ImportSettingsHelper : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        var textureImporter = assetImporter as TextureImporter;
+        if (textureImporter != null)
+        {
+            TextureImporterSettings texSettings = new TextureImporterSettings();
+            textureImporter.compressionQuality = 0;
+            textureImporter.spritePixelsPerUnit = 20;
+            textureImporter.filterMode = FilterMode.Point;
+            if (textureImporter.assetPath.Contains("Chips"))
+            {
+                textureImporter.ReadTextureSettings(texSettings);
+                texSettings.spriteAlignment = (int)SpriteAlignment.Custom;
+                textureImporter.spritePivot = new Vector2(0.5f, 0.425f);
+                textureImporter.SetTextureSettings(texSettings);
+            }
+        }
+    }
+}
