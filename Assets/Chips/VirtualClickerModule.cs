3bec974268ed78511495502f1b175ff4004c9591
Boud <maxime.boudreault.0@gmail.com>
2019-04-29 17:43:41 -0400

    Create virtual clicker module

---
+++ Assets/Chips/VirtualClickerModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualClickerModule : Module
+{
+    [SerializeField] float incomeDelay = 1;
+    [SerializeField] float incomePerTick = 1;
+
+    float accumulator;
+
+    void Update()
+    {
+        if (!IsPowered) return;
+        accumulator += Time.deltaTime;
+        if (accumulator >= incomeDelay)
+        {
+            accumulator -= incomeDelay;
+            Bank.Deposit(incomePerTick);
+        }
+    }
+}
