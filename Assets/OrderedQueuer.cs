1e895010fbce65387ca90743b6bbda850ec4e905
Raphaël Côté <raphael.cote@ellipse-synergie.com>
2019-05-01 22:31:23 -0400

    Add ordered queuer

---
+++ Assets/OrderedQueuer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class OrderedQueuer : SceneSingleton<OrderedQueuer>
+{
+    Dictionary<System.Action, int> queue = new Dictionary<System.Action, int>();
+    public static void Queue(int priority, System.Action action)
+    {
+        Instance.queue.Add(action, priority);
+    }
+
+    public static void Dequeue(System.Action action)
+    {
+        Instance.queue.Remove(action);
+    }
+
+    public static void Move(int newPriority, System.Action action)
+    {
+        Dequeue(action);
+        Queue(newPriority, action);
+    }
+
+    void Start()
+    {
+        foreach (var pair in Instance.queue.OrderBy(k => k.Value))
+        {
+            System.Action action = pair.Key;
+
+            action();
+        }
+    }
+}
