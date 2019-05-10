b522d3a6c2e9e5a215d37b6e8269940263c561cb
Boud <maxime.boudreault.0@gmail.com>
2019-05-10 17:18:54 -0400

    WIP: NotePopUp int to note

--- Assets/Music/NotePopUp.cs
+++ Assets/Music/NotePopUp.cs
@@ -3,5 +3,19 @@ using UnityEngine.UI;
 
 public class NotePopUp : PopUp<int>
 {
-    protected override string Interpret(int value) => value.ToString();
+    protected override string Interpret(int value)
+    {
+        switch (value)
+        {
+            case 0: return "A1";
+            case 1: return "C2";
+            case 2: return "D2";
+            case 3: return "E2";
+            case 4: return "G2";
+            case 5: return "A2";
+            case 6: return "C3";
+            case 7: return "D3";
+            default: return value.ToString();
+        }
+    }
 }
