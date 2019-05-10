8fc2982e186c0510c641f0ffe13af7c5a10876d3
Boud <maxime.boudreault.0@gmail.com>
2019-05-10 11:55:31 -0400

    Add vertical offset to income pop ups

--- Assets/Shop/PopUpMaker.cs
+++ Assets/Shop/PopUpMaker.cs
@@ -3,12 +3,15 @@
 public class PopUpMaker : SceneSingleton<PopUpMaker>
 {
     [SerializeField] TransactionPopUp transactionPrefab = null;
+    [SerializeField] float transactionVerticalOffet = 0.5f;
 
     Pool<TransactionPopUp> transactions;
+    Vector3 transactionOffset;
 
     void Start()
     {
         transactions = new Pool<TransactionPopUp>(() => Instantiate(transactionPrefab, transform));
+        transactionOffset = Vector3.up * transactionVerticalOffet;
     }
 
     public static void ShowTransaction(Vector3 position, decimal amount) => Instance._ShowTransaction(position, amount);
@@ -16,6 +19,6 @@ public class PopUpMaker : SceneSingleton<PopUpMaker>
     {
         var popUp = transactions.Get();
         popUp.transform.SetAsLastSibling();
-        popUp.Show(position, amount);
+        popUp.Show(position + transactionOffset, amount);
     }
 }
